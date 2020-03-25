d14678b5013d88ec815869029ea2eb759a0a8cf6
Martin Heidegger <martin.heidegger@gmail.com>
2020-03-26 01:18:56 +0900

    fix(typescript): fixed timeout declaration

--- index.d.ts
+++ index.d.ts
@@ -2,6 +2,7 @@ type callback<Result> = (error?: Error, data?: Result) => void
 type process<Result> = (unlock: callback<Result>) => void
 type resolve<Result> = (result: Result) => void
 type reject = (error: Error) => void
+type timeout = number
 
 export type Unlock<Result> = callback<Result>
 
@@ -18,7 +19,7 @@ export type CallbacksWithTimeout <T> =
   [timeout, resolve<T>, reject]
 
 export interface FlexLockCbCore {
-  <Result> (process: process<Result>, timeout?: number): Promise<Result>
+  <Result> (process: process<Result>, timeout?: timeout): Promise<Result>
   <Result> (process: process<Result>, ...cb: CallbacksWithTimeout<Result>): void
 }
 
@@ -26,10 +27,10 @@ export interface FlexLockCb extends FlexLockCbCore {
   released(): Promise<void>
   released(onRelease: () => void): void
 
-  syncWrap <Fn extends (...args) => any> (process: Fn, timeout?: number, onError?: (error: Error) => any): (...args: Parameters<Fn>) => void
-  syncWrap <Fn extends (...args) => any> (process: Fn, onError?: (error: Error) => any, timeout?: number): (...args: Parameters<Fn>) => void
+  syncWrap <Fn extends (...args) => any> (process: Fn, timeout?: timeout, onError?: (error: Error) => any): (...args: Parameters<Fn>) => void
+  syncWrap <Fn extends (...args) => any> (process: Fn, onError?: (error: Error) => any, timeout?: timeout): (...args: Parameters<Fn>) => void
 
-  sync <Args extends Array<any>, Result> (process: (lock: this) => Result, timeout?: number): Promise<Result>
+  sync <Args extends Array<any>, Result> (process: (lock: this) => Result, timeout?: timeout): Promise<Result>
   sync <Args extends Array<any>, Result> (process: (lock: this) => Result, ...cb: CallbacksWithTimeout<Result>): void
 
   cb: FlexLockCb
